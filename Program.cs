e0ff5855b12cb5adf825026e4c6781b47a5827ac
JanZelent <janzelentlipinski@gmail.com>
2025-12-29 11:28:25 +0100

    uzupelniona klasa Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using dotenv.net;
+
+class Program
+{
+    static void Main()
+    {
+
+        DotEnv.Load();
+        string? tekst = Environment.GetEnvironmentVariable("TEKST");
+
+        if (string.IsNullOrWhiteSpace(tekst))
+        {
+            Console.WriteLine("Brak wartości TEKST w pliku .env");
+            return;
+        }
+
+        SzyfrCezara szyfr = new SzyfrCezara();
+        WyciaganieEmaila emailer = new WyciaganieEmaila();
+        WykrywaniePolskiego detektor = new WykrywaniePolskiego();
+
+        List<string> odszyfrowania = new List<string>();
+
+        Console.WriteLine("------ Wykonuję brute-force ------\n");
+
+        for (int p = 0; p < 26; p++)
+        {
+            string wynik = szyfr.Odszyfruj(tekst, p);
+            odszyfrowania.Add(wynik);
+            Console.WriteLine($"[{p}] {wynik}");
+            Console.WriteLine("-----------------------------------");
+        }
+
+        Console.WriteLine("\n------ Sprawdzam jezyk przez Google API ------");
+
+        foreach (var wynik in odszyfrowania)
+        {
+            if (detektor.CzyPolski(wynik))
+            {
+                Console.WriteLine($"\n=== Wykryto polski tekst ===\n{wynik}");
+
+                string email = emailer.PobierzEmail(wynik);
+
+                Console.WriteLine("\nEmail:");
+                Console.WriteLine(email);
+
+
+                using (StreamWriter sw = new StreamWriter("wynik.txt"))
+                {
+                    sw.WriteLine("=== Odszyfrowana wiadomosc ===");
+                    sw.WriteLine(wynik);
+                    sw.WriteLine($"\nEmail: {email}");
+                    sw.WriteLine($"\nData wykonania: {DateTime.Now}");
+                }
+
+                Console.WriteLine("\nWynik zapisano do: wynik.txt");
+                return;
+            }
+        }
+
+        Console.WriteLine("\nNie znaleziono tekstu PL.");
+    }
+}
